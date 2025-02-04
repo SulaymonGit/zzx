30ea80ec303c6188daee449c5099f8fe2dfebbc1
SulaymonGit <buuusuuu7@gmail.com>
2025-02-04 10:59:36 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+const ism= prompt("ism qiriting")
+const familiyani= prompt("familiyani qiriting")
+const yosh= +prompt("yosh qiriting")
+
+alert(`name: ${ism}
+    familiyasi${familiyani}
+    yoshi${yosh}`
+
+)
